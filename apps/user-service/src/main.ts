7a8cd54138f04ad45e2953c7299426f7ae6f4868
Amgalan <amka.storm9812@gmail.com>
2024-07-07 02:58:54 +0800

    gt

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello APIss' });
+  res.send({ message: 'Hello APIsaas' });
 });
 
 app.listen(port, host, () => {
